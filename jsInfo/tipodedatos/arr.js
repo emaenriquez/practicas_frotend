ecdf3849aea6a8cb3b44617559c2188dd8f43907
emaenriquez <emaenriquez1985@gmail.com>
2023-12-29 00:20:18 -0300

    ordenando carpetas

---
+++ jsInfo/tipodedatos/arr.js
@@ -0,0 +1,18 @@
+
+let fruits = ["Apple", "Orange", "Plum"];
+
+
+console.log(fruits.at(-1))
+console.log(fruits.pop())
+
+fruits.push('Pear')
+
+console.log(fruits)
+
+console.log(fruits.shift())
+
+console.log(fruits)
+
+fruits.unshift('Apple')
+
+console.log(fruits)
